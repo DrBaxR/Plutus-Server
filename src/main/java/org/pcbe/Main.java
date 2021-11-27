a9d0aeca7e392c497c868a4e5347601794ff8f66
Alexandru Florea <alexflorea16000@gmail.com>
2021-11-27 18:18:17 +0200

    Multithreaded server

---
+++ src/main/java/org/pcbe/Main.java
@@ -0,0 +1,10 @@
+package org.pcbe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.start(4444);
+    }
+
+}
--- src/main/java/org/pcbe/Server.java
+++ src/main/java/org/pcbe/Server.java
@@ -6,42 +6,58 @@ import java.io.*;
 public class Server {
 
     private ServerSocket serverSocket;
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
 
     public void start(int port) {
         try {
             serverSocket = new ServerSocket(port);
-            clientSocket = serverSocket.accept();
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            String greeting = in.readLine();
-            if ("hello server".equals(greeting)) {
-                out.println("hello client");
-            }
-            else {
-                out.println("unrecognised greeting");
-            }
+            while (true)
+                new ClientHandler(serverSocket.accept()).start();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
     public void stop() {
         try {
-            in.close();
-            out.close();
-            clientSocket.close();
             serverSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
-        Server server=new Server();
-        server.start(6666);
+    private static class ClientHandler extends Thread {
+
+        private Socket clientSocket;
+        private PrintWriter out;
+        private BufferedReader in;
+
+        public ClientHandler(Socket socket) {
+            this.clientSocket = socket;
+        }
+
+        public void run() {
+            try {
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                String inputLine;
+                while((inputLine = in.readLine()) != null) {
+                    if (".".equals(inputLine)) {
+                        out.println("bye");
+                        break;
+                    }
+                    out.println(inputLine);
+                }
+
+                in.close();
+                out.close();
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
 }
